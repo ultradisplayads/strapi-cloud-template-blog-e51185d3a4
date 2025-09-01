3980f40d47f2cbacc53e4a79e9ead56c8f4f1858
Anugra Gupta <anugragupta07@gmail.com>
2025-09-01 23:21:46 +0530

    Initial commit: Strapi news backend with multi-source RSS feeds and 5-minute cron job

---
+++ src/api/news-settings/services/news-settings.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * news-settings service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::news-settings.news-settings');
---
+++ src/api/news-source/routes/news-source.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * news-source router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::news-source.news-source');
